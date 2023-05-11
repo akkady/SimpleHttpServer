038ee514907e6b5918bb800c1f9d608f8ae3298a
yakkad <yakkad@sqli.com>
2023-05-11 18:16:44 +0100

    Creating ResponseEntity for a generic response

--- src/main/java/me/akkad/controller/UserController.java
+++ src/main/java/me/akkad/controller/UserController.java
@@ -1,38 +1,44 @@
 package me.akkad.controller;
 
 import me.akkad.http.HttpRequest;
-import me.akkad.middleware.RequestHandler;
+import me.akkad.http.HttpStatusCode;
+import me.akkad.http.ResponseEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserController{
+public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
-    private static final Map<String, String> users = new HashMap<>(
-            Map.of("1", "Marli", "2", "Eren", "3", "Gogo")
-    );
+    private static final List<User> users = getInitialUsers();
     private static final Pattern userIdRegex = Pattern.compile("/users/(?<userId>\\d+)");
 
 
-    public static List<String> getUsers(HttpRequest request) {
+    public static ResponseEntity<List<User>> getUsers(HttpRequest request) {
         log.info("Request to get all users");
-        return new ArrayList<>(users.values());
+        ResponseEntity<List<User>> responseEntity = ResponseEntity.builder(HttpStatusCode.OK, users);
+        responseEntity.setStatusCode(HttpStatusCode.OK);
+        responseEntity.addHeader("Some-Header", "every thing goes ok :)");
+        return responseEntity;
     }
 
-    public static String getUserById(HttpRequest request) {
+    public static ResponseEntity<User> getUserById(HttpRequest request) {
         Matcher matcher = userIdRegex.matcher(request.getRequestTarget());
-        if (matcher.find()) {
-            String usrKey = matcher.group("userId");
-            log.info("Request to get user : {}",usrKey);
-            log.info(usrKey+" : "+users.get(usrKey));
-            return users.get(usrKey);
+        User user = null;
+        if (!matcher.find()) {
+            int userId = Integer.parseInt(matcher.group("userId"));
+            user= users.stream().filter(u -> u.getId() == userId).findAny().orElse(new User());
         }
-        return "";
+        return ResponseEntity.builder(HttpStatusCode.OK, user);
+    }
+
+    private static List<User> getInitialUsers() {
+        return List.of(
+                new User(1, "Eren", 18),
+                new User(1, "Marli", 18),
+                new User(1, "Bejita", 18)
+        );
     }
 }
